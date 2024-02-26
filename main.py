c47026c3fd03ab7fe3d6afbd7d99c0c2064a917a
sheshukoff <sheshukov458@gmail.com>
2024-02-26 14:29:57 +0300

    рефакторинг

--- main.py
+++ main.py
@@ -1,27 +1,21 @@
-# from get_soup_from_city_dodo import parsing_dodo_pizza
-# from get_soup_from_city_tashir import parsing_tashir_pizza
+from working_with_the_user import big_work_with_user
+
+from get_soup_from_city_dodo import parsing_dodo_pizza
+from get_soup_from_city_tashir import parsing_tashir_pizza
 from get_soup_from_city_tomato import parsing_tomato_pizza
 
 
 def main():
-    brands = [{'Додо': ['Абакан', 'Абинск', 'Адлер', 'Азов']},
-              {'Ташир': ['Аксай', 'Астрахань', 'Белгород', 'Волгоград', 'Волжский']},
-              {'Томато': ['Белгород', 'Воронеж', 'Калуга', 'Обнинск', 'Курск', 'Елец']}]
+    brands = big_work_with_user()
 
     for brand in brands:
         for name_brand, cities in brand.items():
             if name_brand == 'Додо':
-                dodo_cities = cities
-                # print(name_brand, dodo_cities)
-                # parsing_dodo_pizza(dodo_cities)
+                parsing_dodo_pizza(name_brand, cities)
             elif name_brand == 'Ташир':
-                tashir_cities = cities
-                # print(name_brand, tashir_cities)
-                # parsing_tashir_pizza(tashir_cities)
+                parsing_tashir_pizza(name_brand, cities)
             elif name_brand == 'Томато':
-                tomato_cities = cities
-                print(tomato_cities)
-                parsing_tomato_pizza(tomato_cities)
+                parsing_tomato_pizza(name_brand, cities)
 
 
 if __name__ == "__main__":
