2b66ec866c19410da52ab2d5679bffb7abfc1b7a
user <sheshukov458@gmail.com>
2024-04-02 20:35:39 +0300

    новый модуль, здесь отбираются бренды и города для парсинга

--- search_brand_and_cities.py
+++ search_brand_and_cities.py
@@ -1,85 +1,50 @@
 from work_with_dash import data
-import numpy as np
-
-import pandas as pd
-
-data = {
-    'города': ['Москва', 'Санкт-Петербург', 'Казань'],
-    'додо': [True, False, True],
-    'ташир': [False, True, False],
-    'томато': [False, False, True]
-}
-
-df = pd.DataFrame(data)
-
-result = df.loc[df['додо'] == True, ['города', 'додо']]
-print(result)
-
-choose_user = [True, False, False, False, True, False, False, False, True, False, False, False, False, False, False,
-               False, True, False, False, False, False, True, True, False, False, False, False, False, False, False,
-               False, False, False, False, False, False, False, False, True, False, False, False, False, False, False]
-
-# part_table = data[:15]
 
 
-split_list = []
+def split_array(choose_user_cities: list) -> list[list]:
+    split_choose_user = []
+    index = 0
 
-# Инициализируем переменную для хранения текущего индекса
-index = 0
+    # Цикл, который берет по три элемента из списка
+    while index < len(choose_user_cities):
+        three_elements = choose_user_cities[index:index + 3]
+        split_choose_user.append(three_elements)
+        index += 3
 
-# Цикл, который берет по три элемента из списка
-while index < len(choose_user):
-    three_elements = choose_user[index:index + 3]
-    split_list.append(three_elements)
-    index += 3
-
-# print(split_list)
-
-
-# split_list = np.array_split(choose_user, 15)
-# [{'Додо': ['Абакан', 'Абинск']}, {'Ташир': ['Аксай', 'Астрахань']}, {'Томато': ['Белгород', 'Воронеж']}]
-
-list_brand_and_cities = []
-
-# for element in choose_user:
-# print(part_table[0]['city'])
-roster_brands = {}
-
-# dodo = element['dodo']
-# tashir = element['tashir']
-# tomato = element['tomato']
-# print(city)
-
-#
-# for number, row in enumerate(part_table):
-#     temp = []
-#     city = row['city']
-#     print(number, city, split_list[number])
-#     brands = split_list[number]
-#     if brands[2] is True:
-#         roster_brands['Додо'] = [city]
-#
-#
-# print(roster_brands)
+    return split_choose_user
 
 
-# for number, row in enumerate(split_list):
-#     dodo, tashir, tomato = row
-#
-#     print(number, dodo, tashir, tomato)
+def search_brand_city(split_choose_user):
+    part_table = data[:15]
+    search_brand_cities = {}
 
-# for num, row in enumerate(split_list):
-#     dodo, tashir, tomato = row
-#     for element in part_table:
-#         print(element['city'])
-#
+    for num, cities in enumerate(part_table):
+        temp = {}
+        for number in range(0, 3):
+            if number == 0:
+                temp['Додо'] = split_choose_user[num][number]
+            elif number == 1:
+                temp['Ташир'] = split_choose_user[num][number]
+            elif number == 2:
+                temp['Томато'] = split_choose_user[num][number]
 
-# for element in part_table:
-#     city = element['city']
+        search_brand_cities[cities['city']] = temp
 
-# print(roster_brands)
+    return search_brand_cities
 
-#     for num_brand, brand in enumerate(split_list[num]):
-#         print(num_brand, brand)
 
-import numpy as np
+def sort_brand_and_city(found_brand_city):
+    found_brand_and_city = {'Додо': [], 'Ташир': [], 'Томато': []}
+
+    for city, brands in found_brand_city.items():
+        if brands['Додо'] is True:
+            print(city, 'Додо')
+            found_brand_and_city['Додо'].append(city)
+        if brands['Ташир'] is True:
+            print(city, 'Ташир')
+            found_brand_and_city['Ташир'].append(city)
+        if brands['Томато'] is True:
+            print(city, 'Томато')
+            found_brand_and_city['Томато'].append(city)
+
+    return found_brand_and_city
