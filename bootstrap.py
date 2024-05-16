1b3e2851f511e566d9d1aef47f4866fee1647f9a
user <sheshukov458@gmail.com>
2024-05-16 19:51:26 +0300

    для тестов

--- bootstrap.py
+++ bootstrap.py
@@ -1,33 +1,40 @@
 import dash
 from dash import html
 from dash import dcc
-from dash.dependencies import Input, Output
-import dash_bootstrap_components as dbc
+from dash.dependencies import Input, Output, State
+import dash_table
+from dash_bootstrap_components import Table
 
-app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+app = dash.Dash(__name__)
 
 app.layout = html.Div([
-    dbc.Checklist(
-        id='toggle-switch',
-        options=[
-            {'label': 'Toggle', 'value': 'toggle'}
+    dash_table.DataTable(
+        id='table',
+        columns=[
+            {'name': 'Город', 'id': 'city'},
+            {'name': 'Бренд 1', 'id': 'brand1', 'presentation': 'dropdown'},
+            {'name': 'Бренд 2', 'id': 'brand2', 'presentation': 'dropdown'},
+            {'name': 'Бренд 3', 'id': 'brand3', 'presentation': 'dropdown'}
         ],
-        value=[]
+        data=[
+            {'city': 'Город 1', 'brand1': False, 'brand2': True, 'brand3': False},
+            {'city': 'Город 2', 'brand1': True, 'brand2': False, 'brand3': True},
+            # Добавьте остальные данные здесь
+        ],
+        page_size=10
     ),
-    html.Div(id='toggle-switch-output')
+    html.Div(id='output-data')
 ])
 
-
 @app.callback(
-    Output('toggle-switch-output', 'children'),
-    [Input('toggle-switch', 'value')]
+    Output('output-data', 'children'),
+    [Input('table', 'data')],
+    [State('table', 'page_current')]
 )
-def update_output(value):
-    if 'toggle' in value:
-        return "Switch is turned on"
-    else:
-        return "Switch is turned off"
+def update_output(data, page_current):
+    return f"Текущая страница: {page_current}, Данные: {data}"
 
 
 if __name__ == '__main__':
-    app.run_server(port=8050)
+    app.run_server(debug=True)
+
