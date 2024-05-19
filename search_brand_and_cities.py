0691a39a06bd52affcbb3670eeb0555b859f3346
user <sheshukov458@gmail.com>
2024-05-19 22:59:31 +0300

    добавлена функция changes_in_data, где происходит изменения значаний переключателей. работает верно.

--- search_brand_and_cities.py
+++ search_brand_and_cities.py
@@ -17,11 +17,11 @@ def search_brand_city(split_choose_user, table):  # здесь состояни�
         temp = {}
         for number in range(0, 3):
             if number == 0:
-                temp['dodo'] = split_choose_user[num][number]
+                temp['dodo_value'] = split_choose_user[num][number]
             elif number == 1:
-                temp['tashir'] = split_choose_user[num][number]
+                temp['tashir_value'] = split_choose_user[num][number]
             elif number == 2:
-                temp['tomato'] = split_choose_user[num][number]
+                temp['tomato_value'] = split_choose_user[num][number]
         search_brand_cities[cities['city']] = temp
 
     return search_brand_cities
@@ -31,11 +31,47 @@ def sort_brand_and_city(found_brand_city):
     found_brand_and_city = {'dodo': [], 'tashir': [], 'tomato': []}
 
     for city, brands in found_brand_city.items():
-        if brands['dodo'] is True:
+        if brands['dodo_value'] is True:
             found_brand_and_city['dodo'].append(city)
-        if brands['tashir'] is True:
+        if brands['tashir_value'] is True:
             found_brand_and_city['tashir'].append(city)
-        if brands['tomato'] is True:
+        if brands['tomato_value'] is True:
             found_brand_and_city['tomato'].append(city)
 
     return found_brand_and_city
+
+
+def changes_in_data(found_brand_and_city, data):
+    print(found_brand_and_city)
+    print(data[:30])
+
+    for cities in data:
+        city_dodo = found_brand_and_city['dodo']
+        city_tashir = found_brand_and_city['tashir']
+        city_tomato = found_brand_and_city['tomato']
+        if len(city_dodo) != 0:
+            for city in city_dodo:
+                if cities['city'] == city:
+                    cities['dodo_value'] = True
+        elif len(city_dodo) == 0:
+            print(f'Список {city_dodo} пустой')
+
+        if len(city_tashir) != 0:
+            for city in city_tashir:
+                if cities['city'] == city:
+                    cities['tashir_value'] = True
+        elif len(city_tashir) == 0:
+            print(f'Список {city_tashir} пустой')
+
+        if len(city_tomato) != 0:
+            for city in city_tashir:
+                if cities['city'] == city:
+                    cities['tomato_value'] = True
+        elif len(city_tomato) == 0:
+            print(f'Список {city_tomato} пустой')
+
+    print(f'Изменения в {data} внесены')
+
+
+
+
