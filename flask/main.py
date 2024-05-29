a67227d3b0f2d4b76f6794a82c83959dac48e397
user <sheshukov458@gmail.com>
2024-05-29 21:11:56 +0300

    сделан переход на станицу dash table и обратно на flask.

--- flask/main.py
+++ flask/main.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_login import login_required, LoginManager, login_user, logout_user
 from flask_bcrypt import Bcrypt
+from werkzeug.middleware.dispatcher import DispatcherMiddleware
+from werkzeug.serving import run_simple
 
-from test_sqlalchemy_flask import recieve_cities
-from load_in_authorized_users import load_table_authorized_users
 from create_table import session, Authorized_users
-from write_query_sql import search_password, recieve_user_login
+from write_query_sql import search_password, recieve_user_login, load_table_authorized_users
+from test_chat_gpt import dash_app
+
 
 app = Flask(__name__)
 app.secret_key = "some secret petelka_458"
@@ -50,9 +52,7 @@ def sign_in():
             flash("Пожалуйста, заполните все поля")
         else:
             login_user_db = recieve_user_login(input_login)  # логин пользователя из БД
-            hash_password_user_db = search_password(
-                input_login
-            )  # хешированный пароль пользоватея из БД
+            hash_password_user_db = search_password(input_login)  # хешированный пароль пользоватея из БД
             if login_user_db is None or hash_password_user_db is None:
                 flash("Логин или пароль не правильный")
                 return redirect(url_for("sign_in"))
@@ -61,7 +61,7 @@ def sign_in():
 
             if input_login == login_user_db and is_valid is True:
                 login_user(user, remember=False)
-                return redirect(url_for("choose_brand", login=input_login))
+                return redirect(url_for("dashboard", login=input_login))
             else:
                 flash("Логин или пароль не правильный")
     else:
@@ -83,12 +83,10 @@ def sign_up():
         elif password != password_repeat:
             flash("Пароли должны быть совпадать")
         else:
-            hashed_password = bcrypt.generate_password_hash(password).decode(
-                "utf-8", "ignore"
-            )
+            hashed_password = bcrypt.generate_password_hash(password).decode("utf-8", "ignore")
             print(login, hashed_password)
 
-            # сдесь происходит загрузка в базу данных login и password (пароль уже хеширован)
+            # здесь происходит загрузка в базу данных login и password (пароль уже хеширован)
             load_table_authorized_users(login, hashed_password)
             return redirect(url_for("sign_in"))
     else:
@@ -103,23 +101,22 @@ def log_out():
 
 
 @login_required
-def choose_brand():
+def dashboard():
+    print("105 строка")
     if request.method == "GET":
         print("GET")
         login = request.args["login"]
     elif request.method == "POST":
         print("POST")
-        return redirect(url_for("choose_city"))
+        button = request.form.get("submit-button")
+        print("110 строка", button)
     else:
         print("another_method")
-
-    content = {"login": login}
-
-    return render_template("choose_brand.html", content=content)
+    return render_template(dash_app)
 
 
 @login_required
-def choose_city():
+def output_parsing_information():
     if request.method == "GET":
         print("GET")
     elif request.method == "POST":
@@ -127,29 +124,62 @@ def choose_city():
     else:
         print("another_method")
 
-    cities = recieve_cities()
 
-    content = {
-        "cities": cities,
-    }
+def page3():
+    return '<h1>Страница 3</h1>'
 
-    return render_template("choose_city.html", content=content)
+
+# @login_required
+# def choose_brand_and_cities():
+#     if request.method == "GET":
+#         print("GET")
+#         login = request.args["login"]
+#     elif request.method == "POST":
+#         print("POST")
+#
+#         return redirect(url_for("choose_city"))
+#     else:
+#         print("another_method")
+#
+#     content = {"login": login}
+#
+#     return render_template("choose_brand.html", content=content)
+
+
+# @login_required
+# def choose_city():
+#     if request.method == "GET":
+#         print("GET")
+#     elif request.method == "POST":
+#         print("POST")
+#     else:
+#         print("another_method")
+#
+#     cities = recieve_cities()
+#
+#     content = {
+#         "cities": cities,
+#     }
+#
+#     return render_template("choose_city.html", content=content)
 
 
 app.add_url_rule("/", "index_page", index_page, methods=["GET", "POST"])
-app.add_url_rule("/choose_brand", "choose_brand", choose_brand, methods=["GET", "POST"])
-app.add_url_rule(
-    "/choose_brand/choose_city", "choose_city", choose_city, methods=["GET", "POST"]
-)
-# app.add_url_rule("/choose_brand/test_choose_city", 'test_choose_city', test_choose_city, methods=['GET', 'POST'])
-
 app.add_url_rule("/sign_in", "sign_in", sign_in, methods=["GET", "POST"])
 app.add_url_rule("/sign_up", "sign_up", sign_up, methods=["GET", "POST"])
+app.add_url_rule("/dashboard/", "dashboard", dashboard, methods=["GET", "POST"])
+app.add_url_rule("/page3", "page3", page3, methods=["GET", "POST"])
+
 app.add_url_rule("/" "log_out", log_out, methods=["GET", "POST"])
 
 
+application = DispatcherMiddleware(
+    app,
+    {"/dashboard": dash_app.server},
+)
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    run_simple("localhost", 5000, application)
 
 # проверить как сейчас работает парсинг
 # перенести функции относящиеся к классу юзер и переписать код
@@ -175,3 +205,9 @@ if __name__ == "__main__":
 #         # cities = recieve_cities()
 #         # print(cities)
 #     return render_template('test_choose_city.html')
+
+
+# app.add_url_rule(
+#     "/choose_brand/choose_city", "choose_city", choose_city, methods=["GET", "POST"]
+# )
+# app.add_url_rule("/choose_brand/test_choose_city", 'test_choose_city', test_choose_city, methods=['GET', 'POST'])
