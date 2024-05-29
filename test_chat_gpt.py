452d9f1567b480493755d040612a7030b9e184f2
user <sheshukov458@gmail.com>
2024-05-29 21:13:37 +0300

    добавлен новый callback переход на другую страницу

--- test_chat_gpt.py
+++ test_chat_gpt.py
@@ -1,16 +1,16 @@
 import dash_bootstrap_components as dbc
-from dash import html, Input, Output, ALL, callback, State
+from dash import html, Input, Output, ALL, callback, State, dcc
 import dash
 from work_with_dash import data
 from components_for_dash_table import get_data_pagination, get_total_page
 from search_brand_and_cities import split_array
 
-app = dash.Dash(__name__,
-                title="Checklist Test",
-                suppress_callback_exceptions=True,
-                external_stylesheets=[dbc.themes.SLATE],
-                url_base_pathname='/dashboard/'
-                )
+dash_app = dash.Dash(__name__,
+                     title="Checklist Test",
+                     suppress_callback_exceptions=True,
+                     external_stylesheets=[dbc.themes.SLATE],
+                     requests_pathname_prefix='/dashboard/'
+                     )
 
 PAGE_SIZE = 15
 previous_page = 1
@@ -35,9 +35,9 @@ def choose_brand_and_cities(number_page: int, button: None | int, choose_user_ci
     for number, values in enumerate(split_choose_user):
         dodo_value, tashir_value, tomato_value = values
 
-        data[start+number]['dodo_value'] = dodo_value
-        data[start+number]['tashir_value'] = tashir_value
-        data[start+number]['tomato_value'] = tomato_value
+        data[start + number]['dodo_value'] = dodo_value
+        data[start + number]['tashir_value'] = tashir_value
+        data[start + number]['tomato_value'] = tomato_value
 
     previous_page = number_page
 
@@ -60,23 +60,35 @@ def table_pagination(number_page: int) -> object:
     return table
 
 
-app.layout = html.Div(
+@dash_app.callback(
+    Output('redirect', 'pathname'),
+    [Input('submit-button', 'n_clicks')],
+    [Input('button-clicks', 'data')]
+)
+def handle_next(n_clicks, button_clicks):
+    if n_clicks > button_clicks:
+        print("Далее")  # Выводим сообщение в консоль
+        return '/page3'  # Перенаправляем на страницу 3
+    return dash.no_update
+
+
+dash_app.layout = html.Div(
     children=[
+        dcc.Store(id='button-clicks', data=0),
         dbc.Table(id="table"),
         dbc.Pagination(
             id="pagination",
             max_value=get_total_page(PAGE_SIZE, len(data)),
             fully_expanded=False,
         ),
-        html.Button('Отправить на парсинг', id='submit-button', n_clicks=0),
-        html.Div(id='container-output-text',
-                 children='Enter a value and press submit'),
+        html.Button('Отправить на парсинг', id='submit-button', className='btn btn-primary', n_clicks=0),
+        dcc.Location(id='redirect', refresh=True)
     ],
     className="table-pagination",
 )
 
 if __name__ == "__main__":
-    app.run_server(debug=True)
+    dash_app.run_server(debug=True)
 
 # https://dash.plotly.com/advanced-callbacks
 # https://dash.plotly.com/determining-which-callback-input-changed
