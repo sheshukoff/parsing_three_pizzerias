d75279a2c5670d67f91738ff41339571f0863fc7
user <sheshukov458@gmail.com>
2024-05-29 21:14:16 +0300

    перенес не которые запросы в один файл

--- flask/write_query_sql.py
+++ flask/write_query_sql.py
@@ -1,6 +1,23 @@
 from create_table import Authorized_users, session, engine
 
 
+def load_table_authorized_users(login: str, hash_password: str):
+    """
+    Функция загружает данные в базу данных, в таблицу "authorized_users".
+    """
+
+    insert_authorized_users = [
+        {"login": login, "password": hash_password},
+    ]
+
+    insert_value = session.scalars(
+        insert(Authorized_users).returning(Authorized_users), insert_authorized_users
+    )
+    result = insert_value.all()
+
+    session.commit()
+
+
 def recieve_user_login(input_login: str):
     """
     Функция ищет пользователя по логину в базе данных
