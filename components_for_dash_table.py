4fd88b60ce392b6e0f5120e1a291f9f20826218c
user <sheshukov458@gmail.com>
2024-06-02 21:34:41 +0300

    перенес функцию "split_array".

--- components_for_dash_table.py
+++ components_for_dash_table.py
@@ -2,6 +2,23 @@ import dash_bootstrap_components as dbc
 from dash import html
 
 
+def split_array(choose_user_cities: list) -> list[list]:
+    split_choose_user = []
+    index = 0
+
+    # Цикл, который берет по три элемента из списка
+    while index < len(choose_user_cities):
+        three_elements = choose_user_cities[index:index + 3]
+        split_choose_user.append(three_elements)
+        index += 3
+
+    return split_choose_user
+
+
+def check_data(data):
+    pass
+
+
 def check_toggle_switch(type_boolean: bool, id_str: str, value: bool) -> object:
     if type_boolean is True:
         return html.Div(
