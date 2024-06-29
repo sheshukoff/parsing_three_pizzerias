7bc01128e16628cb3b053109d9773d2cd63f4e0d
user <sheshukov458@gmail.com>
2024-06-29 13:22:38 +0300

    простой пример с dash table (нужно вписывать адрес самостоятельно)

--- test.py
+++ test.py
@@ -1,91 +1,27 @@
-from werkzeug import run_simple
-from flask import redirect, url_for, request
-from flask import Flask, render_template_string
-from dash import Dash, html, dcc
-from dash.dependencies import Input, Output
+import dash
+import dash_html_components as html
+import flask
 from werkzeug.middleware.dispatcher import DispatcherMiddleware
+from werkzeug.serving import run_simple
 
-# Создаем сервер Flask
-flask_app = Flask(__name__)
-
-
-# Определяем маршруты Flask
-
-def index():
-    if request.method == "GET":
-        print("GET")
-    elif request.method == "POST":
-        print("POST")
-        # return redirect(url_for())
-    return render_template_string('''
-    <h1>Страница 1</h1>
-        <form action = "/dash/page2" method = "post">
-            <p>Есть логин и пароль</p>
-                <input type="submit" class="button sign_in" value="Войти">
-        </form>
-    ''')
+server = flask.Flask(__name__)
 
 
-def page4():
-    return render_template_string('<h1>Страница 4</h1><a href="/">Далее</a>')
+@server.route("/")
+def home():
+    return "Hello, Flask!"
 
 
-# Создаем приложение Dash
-dash_app = Dash(__name__, server=flask_app, routes_pathname_prefix='/dash/')
+app1 = dash.Dash(requests_pathname_prefix="/app1/")
+app1.layout = html.Div("Hello, Dash app 1!")
 
-# Определяем макеты Dash для разных страниц
-page_2_layout = html.Div([
-    html.H1('Страница 2'),
-    html.Table([
-        html.Tr([html.Td('Ячейка 1'), html.Td('Ячейка 2')]),
-        html.Tr([html.Td('Ячейка 3'), html.Td('Ячейка 4')])
-    ]),
-    dcc.Link(html.Button(
-            'Отправить на парсинг', id='submit-button', className='btn btn-primary', n_clicks=0),
-            href='/dash/page3')
-    # html.A('Далее', href='/dash/page3')
-])
+app2 = dash.Dash(requests_pathname_prefix="/app2/")
+app2.layout = html.Div("Hello, Dash app 2!")
 
-page_3_layout = html.Div([
-    html.H1('Страница 3'),
-    html.Table([
-        html.Tr([html.Td('Клетка A'), html.Td('Клетка B')]),
-        html.Tr([html.Td('Клетка C'), html.Td('Клетка D')])
-    ]),
-    html.A(html.Button(
-        'Отправить на парсинг', id='submit-button', className='btn btn-primary', n_clicks=0),
-        href='/page4'),
-])
+application = DispatcherMiddleware(
+    server,
+    {"/app1": app1.server, "/app2": app2.server},
+)
 
-
-# Обработчики для переключения между страницами
-@dash_app.callback(Output('page-content', 'children'),
-                   [Input('url', 'pathname')])
-def display_page(pathname):
-    if pathname == '/dash/page2':
-        return page_2_layout
-    elif pathname == '/dash/page3':
-        return page_3_layout
-    else:
-        return '404'
-
-
-dash_app.layout = html.Div([
-    dcc.Location(id='url', refresh=False),
-    html.Div(id='page-content')
-])
-
-flask_app.add_url_rule("/", "index", index, methods=["GET", "POST"])
-flask_app.add_url_rule("/page4", "page4", page4, methods=["GET", "POST"])
-
-# # Объединяем приложения Flask и Dash
-# application = DispatcherMiddleware(
-#     flask_app,
-#     {
-#         '/dash': dash_app.server,
-#     }
-# )
-
-if __name__ == '__main__':
-    flask_app.run(debug=True)
-    # run_simple("localhost", 5000, application=application)
+if __name__ == "__main__":
+    run_simple("localhost", 8050, application)
