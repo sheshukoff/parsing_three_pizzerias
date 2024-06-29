8dcfc54ea9219882e2c5ef8b80c90ce3dae92c7a
user <sheshukov458@gmail.com>
2024-06-29 13:19:41 +0300

    пример с flask и dash table (не рабочий)

--- bootstrap.py
+++ bootstrap.py
@@ -1,67 +1,116 @@
-from flask import Flask, render_template, redirect, url_for
-from dash import Dash, html, dcc, Input, Output
-import dash_bootstrap_components as dbc
-import dash
+from flask import Flask, render_template_string, url_for
+from dash import Dash, html, dcc
+from dash.dependencies import Input, Output
+from werkzeug.middleware.dispatcher import DispatcherMiddleware
+import flask
+# from werkzeug.wsgi import DispatcherMiddleware
+from werkzeug.exceptions import NotFound
 
-
-# Определяем функцию для первой страницы
-def page1():
-    return '<h1>Страница 1</h1><a href="/page2"><button>Далее</button></a>'
-
-
-# Определяем функцию для третьей страницы
-def page3():
-    return '<h1>Страница 3</h1>'
-
-
-# Создаем Flask-приложение
+# Создаем сервер Flask
 flask_app = Flask(__name__)
 
-# Добавляем правила URL для Flask-приложения
-flask_app.add_url_rule('/', 'page1', page1)
-flask_app.add_url_rule('/page3', 'page3', page3)
+
+# Определяем маршруты Flask
+def index():
+    return render_template_string(
+        '''<h1>Страница 1</h1>
+                <form action = "http://localhost:5000/dash/page2" method = "post">
+                    <p>Пойти на страницу 2</p>
+                        <input type="submit" class="button sign_in" value="Войти">
+                </form>''')
 
 
-# Создаем Dash-приложение
-dash_app = Dash(
-    __name__,
-    server=flask_app,
-    routes_pathname_prefix='/page2/'
-)
+@flask_app.route('/dash/page2', methods=['POST'])
+def method_post():
+    print('24 row')
+    return flask.redirect(url_for('display_page'))
 
 
-# Определяем макет Dash-приложения
-dash_app.layout = html.Div([
+def page4():
+    return render_template_string('<h1>Страница 4</h1><a href="/">Далее</a>')
+
+
+# Создаем приложение Dash
+dash_app = Dash(__name__, server=flask_app, routes_pathname_prefix='/dash/')
+
+# Определяем макеты Dash для разных страниц
+page_2_layout = html.Div([
     html.H1('Страница 2'),
-    dcc.Store(id='button-clicks', data=0),  # Хранилище для счетчика нажатий
-    dbc.Table([
-        html.Thead(html.Tr([html.Th("Колонка 1"), html.Th("Колонка 2")])),
-        html.Tbody([
-            html.Tr([html.Td("Ячейка 1"), html.Td("Ячейка 2")]),
-            html.Tr([html.Td("Ячейка 3"), html.Td("Ячейка 4")])
-        ])
+    html.Table([
+        html.Tr([html.Td('Ячейка 1'), html.Td('Ячейка 2')]),
+        html.Tr([html.Td('Ячейка 3'), html.Td('Ячейка 4')])
     ]),
-    # Используем callback для обработки нажатия кнопки
-    html.Button('Далее', id='next-button', className='btn btn-primary'),
-    dcc.Location(id='redirect', refresh=True)
+    html.Form([
+        html.Button('Отправить на парсинг', id='submit-button', className='btn btn-primary', n_clicks=0),
+    ], action='/dash/page3', method='post'),
+])
+
+page_3_layout = html.Div([
+    html.H1('Страница 3'),
+    html.Table([
+        html.Tr([html.Td('Клетка A'), html.Td('Клетка B')]),
+        html.Tr([html.Td('Клетка C'), html.Td('Клетка D')])
+    ]),
+    html.A('Далее', href='/page4')
 ])
 
 
-# Callback для обработки нажатия кнопки
-@dash_app.callback(
-    Output('redirect', 'pathname'),
-    [Input('next-button', 'n_clicks')],
-    [Input('button-clicks', 'data')]
-)
-def handle_next(n_clicks, button_clicks):
-    if n_clicks > button_clicks:
-        print("Далее")  # Выводим сообщение в консоль
-        return '/page3'  # Перенаправляем на страницу 3
-    return dash.no_update
+# Обработчики для переключения между страницами
+@dash_app.callback(Output('page-content', 'children'),
+                   [Input('url', 'pathname')])
+def display_page(pathname):
+    if pathname == '/dash/page2':
+        return page_2_layout
+    elif pathname == '/dash/page3':
+        return page_3_layout
+    else:
+        return '404'
 
 
-# Запускаем приложение
+@dash_app.server.route('/dash/page3', methods=['POST'])
+def on_post():
+    data = flask.request.form
+    print(data, '62 row')
+    row = list(data)
+    if len(row) == 0:
+        return flask.redirect('/dash/page3')
+    else:
+        return flask.redirect('/dash/page2')
+
+
+dash_app.layout = html.Div(
+    [
+        dcc.Location(id='url', refresh=False),
+        html.Div(id='page-content')
+    ], id='main_div')
+
+
+flask_app.add_url_rule("/", "index", index, methods=["GET", "POST"])
+flask_app.add_url_rule("/dash/page2", "display_page", display_page, methods=["GET"])
+flask_app.add_url_rule("/dash/page3", "display_page", display_page, methods=["GET"])
+flask_app.add_url_rule("/page4", "page4", page4, methods=["GET", "POST"])
+# # Объединяем приложения Flask и Dash
+# dispatcher_application = DispatcherMiddleware(
+#     flask_app,
+#     {
+#         "/dash": dash_app.server,
+#         "/dash": dash_app.server
+#     },
+# )
+
+flask_app.wsgi_app = DispatcherMiddleware(
+    flask_app.wsgi_app,
+    {
+        "/dash": dash_app.server,
+    })
+
+
+# application = DispatcherMiddleware(
+#     server,
+#     {"/app1": app1.server, "/app2": app2.server},
+# )
+
 if __name__ == '__main__':
-    flask_app.run()
+    flask_app.run(debug=True)
 
 # https://dev.to/naderelshehabi/securing-plotly-dash-using-flask-login-4ia2
