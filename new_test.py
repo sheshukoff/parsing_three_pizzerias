3e81474a640b732a95534e22ea1b80b0bc815bf6
user <sheshukov458@gmail.com>
2024-07-06 23:10:39 +0300

    для теста dash table в разных модулях

--- new_test.py
+++ new_test.py
@@ -1,38 +1,27 @@
-import flask
-import dash
-from dash import dcc
-from dash import html
-
-app = dash.Dash(__name__)
-app.secret_key = "some secret petelka_458"
-
-app.layout = html.Form([
-    html.H1('Страница 1'),
-    html.P('Сначала нужно заполнить поле'),
-    dcc.Input(name='name'),
-    html.Button('Submit', type='submit')
-], action='/page_2', method='post')
+from dash import Dash, html, callback, dcc, Input, Output
 
 
-# page_2_layout = html.Form([
-#     html.H1('Страница 2'),
-#     dcc.Input(name='name'),
-#     html.Button('Submit', type='submit')
-# ], action='/', method='post')
+def make_dash(server):
+    return Dash(
+        server=server,
+        url_base_pathname='/dash/'
+    )
 
 
-@app.server.route('/page_2', methods=['POST'])
-def on_post():
-    data = flask.request.form
-    row = dict(data)
-    column = row['name']
-    if len(column) == 0:
-        print(column)
-        return flask.redirect('/')
-    else:
-        print(column)
-        return flask.redirect('/page_2')
+def make_layout():
+    return html.Div(
+        [
+            html.P("Hey this is a Dash app :)"),
+            dcc.Input(id="input"),
+            html.Div(id="output"),
+        ]
+    )
 
 
-if __name__ == '__main__':
-    app.run_server(debug=True, port=7779)
+def define_callbacks():
+    @callback(
+        Output("output", "children"),
+        Input("input", "value"),
+    )
+    def show_output(text):
+        return f"you entered: '{text}'"
