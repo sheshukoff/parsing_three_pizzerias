8afe93d89c2c71dd7ebbfa7b12435bd2a105c70a
user <sheshukov458@gmail.com>
2024-09-06 11:34:54 +0300

    добавил функцию definition_parsing

--- main_for_console.py
+++ main_for_console.py
@@ -5,21 +5,37 @@ from get_soup_from_city_tashir import parsing_tashir_pizza
 from get_soup_from_city_tomato import parsing_tomato_pizza
 
 
+def definition_parsing(name_brand):
+    if name_brand == 'Додо':
+        parsing = parsing_dodo_pizza
+    elif name_brand == 'Ташир':
+        parsing = parsing_tashir_pizza
+    elif name_brand == 'Томато':
+        parsing = parsing_tomato_pizza
+    else:
+        raise f'нет такого бренда {name_brand}'
+
+    return parsing
+
+
 def main():
     brands = big_work_with_user()
     print(brands)
 
     for name_brand, cities in brands.items():
         print(name_brand, cities)
+        parsing = definition_parsing(name_brand)
         if name_brand == 'Додо':
-            for city in cities:
-                parsing_dodo_pizza(name_brand, city)
+            parsing = parsing_dodo_pizza
         elif name_brand == 'Ташир':
-            for city in cities:
-                parsing_tashir_pizza(name_brand, city)
+            parsing = parsing_tashir_pizza
         elif name_brand == 'Томато':
-            for city in cities:
-                parsing_tomato_pizza(name_brand, city)
+            parsing = parsing_tomato_pizza
+        else:
+            raise f'нет такого бренда {name_brand}'
+
+        for city in cities:
+            parsing(name_brand, city)
 
 
 if __name__ == "__main__":
